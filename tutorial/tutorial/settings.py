107f4761b723e45b78d7828728f8981f5358c82e
Robyn Smith <rtasmith05@gmail.com>
2017-08-06 11:21:39 -0400

    Finished: Tutorial 5: Relationships & Hyperlinked APIs
    * http://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django REST Framework settings
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
